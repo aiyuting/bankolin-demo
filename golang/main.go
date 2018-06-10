5023af241b1df1e7a266852cadcc4a4cd50929f7
yeelone <yljckh@gmail.com>
2018-06-10 21:46:35 +0800

    把list改为map，以支持去重功能

--- golang/main.go
+++ golang/main.go
@@ -18,13 +18,16 @@ type Question struct {
 	Answer  string
 }
 
-var questionList []Question
+var questionMap = make(map[string]Question)
 
 func main() {
 	fmt.Println("即将开始！！！")
 	//处理文件
 	handleFile()
 	saveToDB()
+	// for _, q := range questionMap {
+	// 	fmt.Println(q.Subject, strings.Join(q.Options, "||"), q.Answer)
+	// }
 
 }
 
@@ -32,7 +35,7 @@ func saveToDB() {
 	db, err := sql.Open("sqlite3", "./yee.db")
 	checkErr(err)
 
-	for _, q := range questionList {
+	for _, q := range questionMap {
 		stmt, err := db.Prepare(`INSERT INTO question(subject, options, answer) values(?,?,?)`)
 		checkErr(err)
 
@@ -42,12 +45,13 @@ func saveToDB() {
 
 		checkErr(err)
 
-		fmt.Println(id)
+		fmt.Println(id, q.Subject)
 	}
+
 }
 
 func handleFile() {
-	file, err := os.Open("tiku.txt")
+	file, err := os.Open("全.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -72,13 +76,13 @@ func handleFile() {
 
 		if result, s := findAnswer(text); result {
 			q.Answer = s
-			questionList = append(questionList, q)
+			questionMap[q.Subject] = q
 			q = Question{}
 		}
 	}
 }
 func findAnswer(line string) (result bool, s string) {
-	matched, _ := regexp.MatchString(`^(标准答案 ：)`, line)
+	matched, _ := regexp.MatchString(`^(标准答案\s*?：\s*?)`, line)
 	if matched {
 		return true, line
 	}
@@ -86,16 +90,16 @@ func findAnswer(line string) (result bool, s string) {
 }
 
 func findOptions(line string) (result bool, s string) {
+	tempS := strings.TrimSpace(line)
 	//判断选择题
-	reg := regexp.MustCompile(`^\s?(?P<index>[A-Z]\s?)\s?(?P<content>.*)`)
-	matches := reg.FindStringSubmatch(line)
+	reg := regexp.MustCompile(`^\s*?(?P<index>[A-Z]\s?)\s?(?P<content>.*)`)
+	matches := reg.FindStringSubmatch(tempS)
 	if len(matches) > 0 {
 		return true, line
 	}
 
-	reg = regexp.MustCompile(`^(正确)|^(错误)`)
-	matches = reg.FindStringSubmatch(line)
-
+	reg = regexp.MustCompile(`^\s*?(正确)|^\s*?(错误)`)
+	matches = reg.FindStringSubmatch(tempS)
 	if len(matches) > 0 {
 		return true, matches[0]
 	}
@@ -104,30 +108,12 @@ func findOptions(line string) (result bool, s string) {
 }
 
 func findSubject(line string) (result bool, s string) {
-	//reg := regexp.MustCompile(`^\s?(?P<index>\d+ .)\s+(?P<subject>.*)(?P<score>\s+（\d+.\d+分）)$`)
 	reg := regexp.MustCompile(`^\s*?(?P<index>\d+,*?\d*?\s*?\.)\s*?(?P<subject>.*)(?P<score>\s*?（.*分）)$`)
 	matches := reg.FindStringSubmatch(line)
 	if len(matches) > 0 {
 		return true, matches[2]
 	}
 	return false, ""
-
-}
-
-func findSubject2(line string) {
-	reg := regexp.MustCompile(`^\s+(?P<index>\d+ .)\s+(?P<subject>.*)(?P<score>\s+（\d+.\d+分）)$`)
-	str := "2 . （ ）作为银行最主要的服务窗口、产品实现和社会形象展示渠道，是风险防控的重点领域 （1.50分）"
-	matches := reg.FindStringSubmatch(str)
-	for k, v := range matches {
-		log.Printf("%d: %s", k, v)
-	}
-
-	reg2 := regexp.MustCompile(`(?P<score>\s+（\d+.\d+分）)$`)
-	str2 := " 2 . （ ）作为银行最主要的服务窗口、产品实现和社会形象展示渠道，是风险防控的重点领域 （1.50分）"
-	matches2 := reg2.FindStringSubmatch(str2)
-	for k, v := range matches2 {
-		log.Printf("new : %d: %s", k, v)
-	}
 
 }
 
